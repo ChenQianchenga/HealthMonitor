a07b864d0f13781c2e602715a6b63811438299ef
Nanfeng-lll <1025212193@qq.com>
2024-04-06 21:21:47 +0800

    更改错别字

--- HealthMonitor/emails.py
+++ HealthMonitor/emails.py
@@ -78,16 +78,6 @@ def send_manual_alert_clearance_email(position, first_data):
     send_mail(subject='Alert Clearance', to=to, body="body")
 
 
-def send_email_test():
-    recipients = ['986508902@qq.com', '404413567@qq.com']  # 多个收件人地址
-    source = string.digits * 4
-    captcha = random.sample(source, 4)
-    captcha = "".join(captcha)
-    message = Message(subject="知了传课注册验证码", recipients=recipients, body=f"告警了啊啊啊啊：{captcha}")
-    mail.send(message=message)
-    print("邮件已经发送")
-
-
 # 加速器和陀螺仪判断老人摔倒自动报警
 def send_automatic_monitoring_alert_email(position, **kwargs):
     to = ['1025212193@qq.com', '986508902@qq.com', '404413567@qq.com']
