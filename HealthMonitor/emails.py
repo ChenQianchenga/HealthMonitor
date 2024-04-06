d18f50b033d51c04d436ece9b6549e61efb849e0
ChenQianchenga <980938541@qq.com>
2024-04-06 12:39:21 +0800

    邮件增加更多信息2

--- HealthMonitor/emails.py
+++ HealthMonitor/emails.py
@@ -12,6 +12,7 @@ from threading import Thread
 from flask import current_app
 from flask_mail import Message
 from HealthMonitor.extensions import mail
+from loguru import logger
 
 
 def _send_async_mail(app, message):
@@ -28,47 +29,53 @@ def send_mail(subject, to, body):
 
 
 # 老人主动点击按钮触发告警
-def send_manual_alert_email(position):
+def send_manual_alert_email(position, first_data):
     to = ['1025212193@qq.com', '986508902@qq.com', '404413567@qq.com']
-    body = f"""
-                Monitoring Alert!
-
-                An elderly individual has experienced a fall, as detected by the accelerometer and gyroscope.
-
-                **Location:** {position}
-
-                **Environmental Conditions:**
-                - Temperature: {temperature}°C
-
-                **Vital Signs:**
-                - Blood Oxygen Saturation: {blood_oxygen}% 
-                - Heart Rate: {heart_rate} bpm
-
-                Immediate attention is required. Please respond promptly.
-                """
-    send_mail(subject='Alert', to=to, body=body)
+    logger.info(f'数据库中最新一条数据的对象为：{first_data}')
+    # body = f"""
+    #             Monitoring Alert!
+    #
+    #             An elderly individual has experienced a fall, as detected by the accelerometer and gyroscope.
+    #
+    #             **Location:** {position}
+    #
+    #             **Environmental Conditions:**
+    #             - Temperature: {environment_temperature}°C
+    #             - Humidity: {environment_humidity}%
+    #
+    #             **Body Vital Signs:**
+    #             - Temperature: {body_temperature}°C
+    #             - Blood Oxygen Saturation: {blood_oxygen}%
+    #             - Heart Rate: {heart_rate} bpm
+    #
+    #             Immediate attention is required. Please respond promptly.
+    #         """
+    send_mail(subject='Alert', to=to, body="body")
 
 
 # 老人主动点击按钮解除告警
-def send_manual_alert_clearance_email(position):
+def send_manual_alert_clearance_email(position, first_data):
     to = ['1025212193@qq.com', '986508902@qq.com', '404413567@qq.com']
-    body = f"""
-                Monitoring Alert!
-
-                An elderly individual has experienced a fall, as detected by the accelerometer and gyroscope.
-
-                **Location:** {position}
-
-                **Environmental Conditions:**
-                - Temperature: {temperature}°C
-
-                **Vital Signs:**
-                - Blood Oxygen Saturation: {blood_oxygen}% 
-                - Heart Rate: {heart_rate} bpm
-
-                Immediate attention is required. Please respond promptly.
-                """
-    send_mail(subject='Alert Clearance', to=to, body=body)
+    logger.info(f'数据库中最新一条数据的对象为：{first_data}')
+    # body = f"""
+    #             Monitoring Alert!
+    #
+    #             An elderly individual has experienced a fall, as detected by the accelerometer and gyroscope.
+    #
+    #             **Location:** {position}
+    #
+    #             **Environmental Conditions:**
+    #             - Temperature: {environment_temperature}°C
+    #             - Humidity: {environment_humidity}%
+    #
+    #             **Body Vital Signs:**
+    #             - Temperature: {body_temperature}°C
+    #             - Blood Oxygen Saturation: {blood_oxygen}%
+    #             - Heart Rate: {heart_rate} bpm
+    #
+    #             Immediate attention is required. Please respond promptly.
+    #         """
+    send_mail(subject='Alert Clearance', to=to, body="body")
 
 
 def send_email_test():
@@ -84,22 +91,25 @@ def send_email_test():
 # 加速器和陀螺仪判断老人摔倒自动报警
 def send_automatic_monitoring_alert_email(position, **kwargs):
     to = ['1025212193@qq.com', '986508902@qq.com', '404413567@qq.com']
+    logger.info(f"调用主动报警邮件：{kwargs}")
 
     body = f"""
-            Monitoring Alert!
-    
-            An elderly individual has experienced a fall, as detected by the accelerometer and gyroscope.
-    
-            **Location:** {position}
-    
-            **Environmental Conditions:**
-            - Temperature: {kwargs['temperature']}°C
-    
-            **Vital Signs:**
-            - Blood Oxygen Saturation: {kwargs['blood_oxygen']}% 
-            - Heart Rate: {kwargs['heart_rate']} bpm
-    
-            Immediate attention is required. Please respond promptly.
+                Monitoring Alert!
+
+                An elderly individual has experienced a fall, as detected by the accelerometer and gyroscope.
+
+                **Location:** {position}
+
+                **Environmental Conditions:**
+                - Temperature: {kwargs['temperature']}°C
+                - Humidity: {kwargs['humidity']}%
+
+                **Body Vital Signs:**
+                - Temperature: {kwargs['temp']}°C
+                - Blood Oxygen Saturation: {kwargs['spo2']}%
+                - Heart Rate: {kwargs['bmp']} bpm
+
+                Immediate attention is required. Please respond promptly.
             """
 
     send_mail(subject='Monitoring Alert', to=to, body=body)
